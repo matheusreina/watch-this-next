284009659a56d860677db26e0b8b237a025df981
Matheus Reina <matheushfreina@gmail.com>
2024-10-18 15:49:28 -0300

    UPDATE: Updated all Frontend Services

--- frontend/src/app/services/season-requests.service.ts
+++ frontend/src/app/services/season-requests.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeasonRequestsService {
+  private readonly apiUrl = 'http://localhost:8080/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSeasons(language: string, id: string) {
+    return this.http.get(`${this.apiUrl}${language}/tv/${id}/seasons`);
+  }
+
+  getEpisodes(language: string, id: string, seasonNumber: number) {
+    return this.http.get(`${this.apiUrl}${language}/tv/${id}/season-${seasonNumber}`);
+  }
 }
